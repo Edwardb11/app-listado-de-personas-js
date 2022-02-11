5d90edf4ceb75d8cd8c1942d159779889a59ac3d
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-11 10:20:11 -0400

    reset value

--- js/app.js
+++ js/app.js
@@ -1,50 +1,45 @@
-class Persona{
-    constructor(nombre, apellido){
-        this._nombre = nombre;
-        this._apellido = apellido;
-    }
-    get nombre(){
-        return this._nombre;
-    }
-    set nombre(nombre){
-        this._nombre = nombre;
-    }
-    get apellido(){
-        return this._apellido;
-    }
-    set apellido(apellido){
-        return this._apellido = apellido;
-    }
-    
+class Persona {
+  constructor(nombre, apellido) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+  get apellido() {
+    return this._apellido;
+  }
+  set apellido(apellido) {
+    return (this._apellido = apellido);
+  }
 }
-const personas = [
-    new Persona('Juan', 'Perez'),
-    new Persona('Karla', 'Lara')
-];
+const personas = [new Persona("Juan", "Perez"), new Persona("Karla", "Lara")];
 
-function mostrarPersonas(){
-    console.log('Mostrar personas...');
-    let texto = '';
-    for(let persona of personas){
-        console.log(persona);
-        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
-    }
-    document.getElementById('personas').innerHTML = texto;
+function mostrarPersonas() {
+  console.log("Mostrar personas...");
+  let texto = "";
+  for (let persona of personas) {
+    console.log(persona);
+    texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+  }
+  document.getElementById("personas").innerHTML = texto;
 }
 
-function agregarPersona(){
-    const forma = document.forms['forma'];
-    const nombre = forma['nombre'];
-    const apellido = forma['apellido'];
-    if(nombre.value != '' && apellido.value != ''){
-        const persona = new Persona(nombre.value, apellido.value);
-        console.log(persona);
-        personas.push(persona);
-        mostrarPersonas();
-        nombre.value=''
-        apellido.value=''
-    }
-    else{
-        console.log('No hay información que agregar');
-    }
+function agregarPersona() {
+  const forma = document.forms["forma"];
+  const nombre = forma["nombre"];
+  const apellido = forma["apellido"];
+  if (nombre.value != "" && apellido.value != "") {
+    const persona = new Persona(nombre.value, apellido.value);
+    console.log(persona);
+    personas.push(persona);
+    nombre.value = "";
+    apellido.value = "";
+    mostrarPersonas();
+  } else {
+    console.log("No hay información que agregar");
+  }
 }
