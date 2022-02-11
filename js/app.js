d37618b19461e44b2fd363a552a48c216d705669
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-11 09:54:42 -0400

    Agregando las clases persona en js y la funcion para mostrar personas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('Listado de Personas');
+const personas = [
+  new Persona("Edward", "Brito"),
+  new Persona("Carlos", "Lara"),
+];
+
+function mostrarPersonas() {
+  console.log("Mostrar personas...");
+  let texto = "";
+  for (let persona of personas) {
+    console.log(persona);
+    texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+  }
+  document.getElementById("personas").innerHTML = texto;
+}
