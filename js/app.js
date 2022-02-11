1e82647f84fd039a8f779047c05e52200676f7d0
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-11 10:12:44 -0400

    formulario para agregar persona y uniendo la clase persona a app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,48 @@
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        return this._apellido = apellido;
+    }
+    
+}
 const personas = [
-  new Persona("Edward", "Brito"),
-  new Persona("Carlos", "Lara"),
+    new Persona('Juan', 'Perez'),
+    new Persona('Karla', 'Lara')
 ];
 
-function mostrarPersonas() {
-  console.log("Mostrar personas...");
-  let texto = "";
-  for (let persona of personas) {
-    console.log(persona);
-    texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
-  }
-  document.getElementById("personas").innerHTML = texto;
+function mostrarPersonas(){
+    console.log('Mostrar personas...');
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
+
+function agregarPersona(){
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if(nombre.value != '' && apellido.value != ''){
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona);
+        mostrarPersonas();
+    }
+    else{
+        console.log('No hay información que agregar');
+    }
 }
--- js/persona.js
+++ js/persona.js
@@ -1,18 +1,19 @@
-class Persona {
-  constructor(nombre, apellido) {
-    this._nombre = nombre;
-    this._apellido = apellido;
-  }
-  get nombre() {
-    return this._nombre;
-  }
-  set nombre(nombre) {
-    this._nombre = nombre;
-  }
-  get apellido() {
-    return this._apellido;
-  }
-  set apellido(apellido) {
-    return (this._apellido = apellido);
-  }
-}
+// class Persona{
+//     constructor(nombre, apellido){
+//         this._nombre = nombre;
+//         this._apellido = apellido;
+//     }
+//     get nombre(){
+//         return this._nombre;
+//     }
+//     set nombre(nombre){
+//         this._nombre = nombre;
+//     }
+//     get apellido(){
+//         return this._apellido;
+//     }
+//     set apellido(apellido){
+//         return this._apellido = apellido;
+//     }
+    
+// }
